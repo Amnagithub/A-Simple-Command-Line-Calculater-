cee1b1b7eb58cbdc9d68f1042fa3ded6bd45d1b6
Amnagithub <amnaaplus1@gmail.com>
2024-03-19 17:55:41 +0500

    second commit

---
+++ index.ts
@@ -0,0 +1,40 @@
+// A Simple Cammand Line Calculater:
+
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+  { message: "Enter First Number", type: "number", name: "fstnumber" },
+  { message: "Enter Second Number", type: "number", name: "sndnumber" },
+  {
+    message: "Select one of the operators to perform action",
+    type: "list",
+    name: "operator",
+    choices: [
+      "Addition",
+      "Subtraction",
+      "Multipilication",
+      "Division",
+      "Modulus",
+      "Exponentiation",
+    ],
+  },
+]);
+// console.log(answer);
+
+// add conditional statments
+
+if (answer.operator === "Addition") {
+  console.log(answer.fstnumber + answer.sndnumber);
+} else if (answer.operator === "Subtraction") {
+  console.log(answer.fstnumber - answer.sndnumber);
+} else if (answer.operator === "Multipilication") {
+  console.log(answer.fstnumber * answer.sndnumber);
+} else if (answer.operator === "Division") {
+  console.log(answer.fstnumber / answer.sndnumber);
+} else if (answer.operator === "Modulus") {
+  console.log(answer.fstnumber % answer.sndnumber);
+} else if (answer.operator === "Exponentiation") {
+  console.log(answer.fstnumber ** answer.sndnumber);
+} else {
+  console.log("Select valid operation");
+}
